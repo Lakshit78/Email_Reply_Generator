022b95d492added1d40b7274073ad237514474b9
Lakshit78 <lakshitajaygupta@gmail.com>
2025-05-02 13:54:51 +0530

    made the project folder

---
+++ email-writer/src/main/java/org/email/writer/EmailWriterApplication.java
@@ -0,0 +1,13 @@
+package org.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailWriterApplication.class, args);
+    }
+
+}
